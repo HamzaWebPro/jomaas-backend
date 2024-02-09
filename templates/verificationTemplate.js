1f17847adfb55567d0b27c14acdeb5417cc17cc9
HamzaWebPro <hamza.mern2201@gmail.com>
2024-02-09 14:26:57 +0600

    addedd live li9nk on validation template

--- templates/verificationTemplate.js
+++ templates/verificationTemplate.js
@@ -1,5 +1,5 @@
 const verificationTemplate = (verificationToken) => {
-  return `<p>Please click the following link to verify your account: <a href="http://localhost:3000/verify?token=${verificationToken}"> Verify Account </a> </p>`;
+  return `<p>Please click the following link to verify your account: <a href="https://jomaas-order.vercel.app/verify?token=${verificationToken}"> Verify Account </a> </p>`;
 };
 
 module.exports = verificationTemplate;
